26d8bf986a7622a7fc3bc3e05a6f3335c28dfabe
Gourav Sahu <gs2237149@gmail.com>
2025-03-06 15:43:26 +0530

    Create Q02.java

---
+++ OOPJ/Assignment-2/Q02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOddBitwise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        if ((n & 1) == 0) {
+            System.out.println(n + " is Even");
+        } else {
+            System.out.println(n + " is Odd");
+        }
+
+        scanner.close();
+    }
+}

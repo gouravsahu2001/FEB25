eb648aeb4436923317d49a64bf69abf6dc2d6d3a
Gourav Sahu <gs2237149@gmail.com>
2025-03-06 15:50:43 +0530

    Create Q03.java

---
+++ OOPJ/Assignment-2/Q03.java
@@ -0,0 +1,11 @@
+public class Q03{
+	public static void main(String[] args){
+		int n = 123456;
+		int result = 0;
+		while(x!=0){
+			res = result + (x%10);
+			x = x/10;
+		}
+		System.out.println("Sum: " + result);
+	}
+}

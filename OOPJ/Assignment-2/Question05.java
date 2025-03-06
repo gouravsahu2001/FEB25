a73bf4c26f283c1c6646a5e3f1148c612a69f2fb
Gourav Sahu <gs2237149@gmail.com>
2025-03-06 22:52:31 +0530

    Added

--- OOPJ/Assignment-2/Question05.java
+++ OOPJ/Assignment-2/Question05.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
-
-public class Question05 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        
-        int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-        
-        System.out.println("The largest number is: " + largest);
-        scanner.close();
-    }
+public class Question05{
+	public static void main(String[] args){
+		int a = 3, b = 9;
+		System.out.println("Before Swapping: \nA = " + a + "\nB = " + b); 
+		a += b;
+		b -= a;
+		b = -b;
+		a -= b;
+		System.out.println("After Swapping: \nA = " + a + "\nB = " + b); 
+	}
 }

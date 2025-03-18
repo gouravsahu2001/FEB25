2d5f80f85be2c1823891a555053fac688e37c904
Gourav Sahu <gs2237149@gmail.com>
2025-03-19 00:35:55 +0530

    IOExceptionChecked.java

---
+++ OOPJ/HW/Day 12/IOExceptionChecked.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.io.FileReader;
+
+public class IOExceptionChecked {
+	public static void main(String args[]) {
+		String fileName = "XYZ.txt"; // File that does not exist
+
+        try {
+            
+            FileReader fileReader = new FileReader(fileName);//Exception
+            System.out.println("File opened successfully.");
+        } catch (FileNotFoundException e) {
+            
+            System.out.println("Error: The file '" + fileName + "' was not found.");
+        }catch(Exception e) {
+        	System.out.println("Error: The file " + fileName +" was not found.");
+        }
+	}
+
+}
